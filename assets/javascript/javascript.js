cca2bc21e965cbd4f78d08f33c920c07f6d41309
Cosmin Sofletea <cozo@Cosmins-MBP.home>
2017-05-12 22:18:20 -0400

    everything works need to do some styling

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,5 +1,40 @@
 var correctAnswer = 0;
 var incorrectAnswer = 0;
+var timer = 31;  
+  
+  function runTimer() {
+
+      intervalId = setInterval(decrement, 1000);
+
+    }
+
+    //  The decrement function.
+    function decrement() {
+
+      //  Decrease number by one.
+      timer--;
+
+      //  Show the number in the .timerDiv tag.
+      $('#timer').html(timer + " Seconds Remaining!");
+
+
+      // When the time runs out
+      if (timer === 0) {
+          stop();
+          timerAnswer();
+          $('.gameArea').hide();
+    $('.results').show();
+    
+       }
+    }
+
+    //  The stop function
+    function stop() {
+
+      //  to the clearInterval function.
+      clearInterval(intervalId);
+
+    }
 
 
 //Hide the Questions untill user clicks start game.           
@@ -9,10 +44,44 @@ $('.results').hide();
 $("#startButton").on("click", function() {
     $(".gameArea").show();
     $(".startGame").hide();
+    runTimer();
 
 });
 
+function timerAnswer() {
+    if ($('input[name="answer"]:checked').val() === "correct") {
+        correctAnswer++;
+    } else if ($('input[name="answer"]:checked').val() === "wrong") {
+        incorrectAnswer++;
+    }
+    if ($('input[name="answer2"]:checked').val() === "correct") {
+        correctAnswer++;
+    } else if ($('input[name="answer2"]:checked').val() === "wrong") {
+        incorrectAnswer++;
+    }
+    if ($('input[name="answer3"]:checked').val() === "correct") {
+        correctAnswer++;
+    } else if ($('input[name="answer3"]:checked').val() === "wrong") {
+        incorrectAnswer++;
+    }
+    if ($('input[name="answer4"]:checked').val() === "correct") {
+        correctAnswer++;
+    } else if ($('input[name="answer4"]:checked').val() === "wrong") {
+        incorrectAnswer++;
+    }
+    if ($('input[name="answer5"]:checked').val() === "correct") {
+        correctAnswer++;
+    } else if ($('input[name="answer5"]:checked').val() === "wrong") {
+        incorrectAnswer++;
+    }
 
+    //display results area and hide the game area
+    $('.gameArea').hide();
+    $('.results').show();
+    $('.correctAnswers').text("Correct Answers : " + correctAnswer);
+    $('.incorrectAnswers').text("Incorrect Answers : " + incorrectAnswer);
+
+}
 
 
 // Check if the answers are correct or incorrect and add to counters
@@ -50,15 +119,24 @@ $(".submitButton").click(function() {
     $('.incorrectAnswers').text("Incorrect Answers : " + incorrectAnswer);
 });
 
+function clearForm(){
+  $('input[type=radio]').prop('checked',false);
+  
+ }
+
 
 //reset function
 
 function resetGame() {
-    var correctAnswer = 0;
-    var incorrectAnswer = 0;
+    correctAnswer = 0;
+    incorrectAnswer = 0;
+    timer = 31;
     $(".gameArea").hide();
     $('.results').hide();
     $('.startGame').show();
+    stop();
+    clearForm();
+    
   }
 
     $("#restartButton").click(function() {
