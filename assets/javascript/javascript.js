0414b8222081295e9829bc9aad92c0596573c2ce
Cosmin Sofletea <cozo@Cosmins-MBP.home>
2017-05-13 15:34:20 -0400

    works fine

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,40 +1,41 @@
 var correctAnswer = 0;
 var incorrectAnswer = 0;
-var timer = 30;  
-  
-  function runTimer() {
+var unanswered = 0;
+var timer = 30;
 
-      intervalId = setInterval(decrement, 1000);
+function runTimer() {
 
-    }
+    intervalId = setInterval(decrement, 1000);
 
-    //  The decrement function.
-    function decrement() {
+}
 
-      //  Decrease number by one.
-      timer--;
+//  The decrement function.
+function decrement() {
 
-      //  Show the number in the .timerDiv tag.
-      $('#timer').html(timer);
+    //  Decrease number by one.
+    timer--;
+
+    //  Show the number in the .timerDiv tag.
+    $('#timer').html(timer);
 
 
-      // When the time runs out
-      if (timer === 0) {
-          stop();
-          timerAnswer();
-          $('.gameArea').hide();
-    $('.results').show();
-    
-       }
-    }
-
-    //  The stop function
-    function stop() {
-
-      //  to the clearInterval function.
-      clearInterval(intervalId);
+    // When the time runs out
+    if (timer === 0) {
+        stop();
+        timerAnswer();
+        $('.gameArea').hide();
+        $('.results').show();
 
     }
+}
+
+//  The stop function
+function stop() {
+
+    //  to the clearInterval function.
+    clearInterval(intervalId);
+
+}
 
 
 //Hide the Questions untill user clicks start game.           
@@ -44,6 +45,7 @@ $('.results').hide();
 $("#startButton").on("click", function() {
     $(".gameArea").show();
     $(".startGame").hide();
+    $('#timer').html(timer);
     runTimer();
 
 });
@@ -53,26 +55,36 @@ function timerAnswer() {
         correctAnswer++;
     } else if ($('input[name="answer"]:checked').val() === "wrong") {
         incorrectAnswer++;
+    } else if ($('input:radio[checked=false]')) {
+        unanswered++;
     }
     if ($('input[name="answer2"]:checked').val() === "correct") {
         correctAnswer++;
     } else if ($('input[name="answer2"]:checked').val() === "wrong") {
         incorrectAnswer++;
+    } else if ($('input:radio[checked=false]')) {
+        unanswered++;
     }
     if ($('input[name="answer3"]:checked').val() === "correct") {
         correctAnswer++;
     } else if ($('input[name="answer3"]:checked').val() === "wrong") {
         incorrectAnswer++;
+    } else if ($('input:radio[checked=false]')) {
+        unanswered++;
     }
     if ($('input[name="answer4"]:checked').val() === "correct") {
         correctAnswer++;
     } else if ($('input[name="answer4"]:checked').val() === "wrong") {
         incorrectAnswer++;
+    } else if ($('input:radio[checked=false]')) {
+        unanswered++;
     }
     if ($('input[name="answer5"]:checked').val() === "correct") {
         correctAnswer++;
     } else if ($('input[name="answer5"]:checked').val() === "wrong") {
         incorrectAnswer++;
+    } else if ($('input:radio[checked=false]')) {
+        unanswered++;
     }
 
     //display results area and hide the game area
@@ -80,7 +92,7 @@ function timerAnswer() {
     $('.results').show();
     $('.correctAnswers').text("Correct Answers : " + correctAnswer);
     $('.incorrectAnswers').text("Incorrect Answers : " + incorrectAnswer);
-
+    $('.unanswered').text("Unanswered : " + unanswered);
 }
 
 
@@ -90,26 +102,36 @@ $(".submitButton").click(function() {
         correctAnswer++;
     } else if ($('input[name="answer"]:checked').val() === "wrong") {
         incorrectAnswer++;
+    } else if ($('input:radio[checked=false]')) {
+        unanswered++;
     }
     if ($('input[name="answer2"]:checked').val() === "correct") {
         correctAnswer++;
     } else if ($('input[name="answer2"]:checked').val() === "wrong") {
         incorrectAnswer++;
+    } else if ($('input:radio[checked=false]')) {
+        unanswered++;
     }
     if ($('input[name="answer3"]:checked').val() === "correct") {
         correctAnswer++;
     } else if ($('input[name="answer3"]:checked').val() === "wrong") {
         incorrectAnswer++;
+    } else if ($('input:radio[checked=false]')) {
+        unanswered++;
     }
     if ($('input[name="answer4"]:checked').val() === "correct") {
         correctAnswer++;
     } else if ($('input[name="answer4"]:checked').val() === "wrong") {
         incorrectAnswer++;
+    } else if ($('input:radio[checked=false]')) {
+        unanswered++;
     }
     if ($('input[name="answer5"]:checked').val() === "correct") {
         correctAnswer++;
     } else if ($('input[name="answer5"]:checked').val() === "wrong") {
         incorrectAnswer++;
+    } else if ($('input:radio[checked=false]')) {
+        unanswered++;
     }
 
     //display results area and hide the game area
@@ -117,12 +139,13 @@ $(".submitButton").click(function() {
     $('.results').show();
     $('.correctAnswers').text("Correct Answers : " + correctAnswer);
     $('.incorrectAnswers').text("Incorrect Answers : " + incorrectAnswer);
+    $('.unanswered').text("Unanswered : " + unanswered);
 });
 
-function clearForm(){
-  $('input[type=radio]').prop('checked',false);
-  
- }
+function clearForm() {
+    $('input[type=radio]').prop('checked', false);
+
+}
 
 
 //reset function
@@ -130,18 +153,17 @@ function clearForm(){
 function resetGame() {
     correctAnswer = 0;
     incorrectAnswer = 0;
+    unanswered = 0;
     timer = 30;
     $(".gameArea").hide();
     $('.results').hide();
     $('.startGame').show();
-    clearInterval(intervalId);
+    $('#timer').html(timer);
     stop();
     clearForm();
-    
-  }
 
-    $("#restartButton").click(function() {
-                resetGame();
-            });
+}
 
-
+$("#restartButton").click(function() {
+    resetGame();
+});
